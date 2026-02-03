0abe6d9de20a0b8cf2b3f8e0a6af573952a7f998
Roberto <gvam1620@gmail.com>
2026-02-03 07:33:30 +0000

    first commit

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { NatsModule } from './transport/nats.module';
+import { TcpModule } from './transport/tcp.module';
+
+
+@Module({
+  imports: [ProductsModule, OrdersModule, NatsModule, TcpModule],
+})
+export class AppModule {}
